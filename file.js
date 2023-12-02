ca486deefe32f85fb558b9fa34b00eec23d04bcb
asif7480 <a.a.khan7480@gmail.com>
2023-12-02 13:04:08 +0500

    compare page

---
+++ file.js
@@ -0,0 +1,101 @@
+var product = [{
+        "name": "",
+        "detail": "",
+        "price": "",
+        "description": "",
+        "brand": "",
+        "image": "images/0.png"
+    },
+    {
+        "name": "iPhone 13",
+        "detail": "The latest iPhone from Apple",
+        "price": "12000",
+        "description": "Snapdragon 888 (USA)/Exynos 2100 (Global)",
+        "brand": "I-Phone",
+        "image": "images/a1.jpg",
+        "path": "iphone.html"
+    },
+    {
+        "name": "Samsung Galaxy S21",
+        "detail": "Flagship smartphone from Samsung",
+        "price": "11000",
+        "description": "Snapdragon 888 (USA)/Exynos 2100 (Global)",
+        "brand": "Samsung",
+        "image": "images/a2.jpg",
+        "path": "samsung.html"
+    },
+    {
+        "name": "OnePlus 9 Pro",
+        "detail": "Flagship killer from OnePlus",
+        "price": "10000",
+        "description": "Snapdragon 888",
+        "brand": "OnePlus",
+        "image": "images/a3.jpg"
+    },
+    {
+        "name": "Google Pixel 6",
+        "detail": "Flagship from Google with advanced camera capabilities",
+        "price": "9000",
+        "description": "Google Tensor",
+        "brand": "Google",
+        "image": "images/a4.jpg"
+    },
+    {
+        "name": "Xiaomi Mi 11",
+        "detail": "Feature-packed flagship from Xiaomi",
+        "price": "8000",
+        "description": "Snapdragon 888",
+        "brand": "Xiaomi",
+        "image": "images/a5.jpg"
+    },
+    {
+        "name": "NOKIA 3310",
+        "detail": "Feature-packed flagship from NOKIA",
+        "price": "23000",
+        "description": "Snapdragon 888",
+        "brand": "NOKIA",
+        "image": "images/a6.jpg"
+    }, {
+        "name": "Techno Spark S1",
+        "detail": "Feature-packed flagship from NOKIA",
+        "price": "23000",
+        "description": "Snapdragon 888",
+        "brand": "NOKIA",
+        "image": "images/a7.jpg"
+    }, {
+        "name": "Techno Spark S2",
+        "detail": "Feature-packed flagship from NOKIA",
+        "price": "23000",
+        "description": "Snapdragon 888",
+        "brand": "NOKIA",
+        "image": "images/a8.jpg"
+    }, {
+        "name": "Techno Spark S3",
+        "detail": "Feature-packed flagship from NOKIA",
+        "price": "23000",
+        "description": "Snapdragon 888",
+        "brand": "NOKIA",
+        "image": "images/a9.jpg"
+    }, {
+        "name": "Techno Spark S4",
+        "detail": "Feature-packed flagship from NOKIA",
+        "price": "23000",
+        "description": "Snapdragon 888",
+        "brand": "NOKIA",
+        "image": "images/a4.jpg"
+    }, {
+        "name": "Techno Spark S4",
+        "detail": "Feature-packed flagship from NOKIA",
+        "price": "23000",
+        "description": "Snapdragon 888",
+        "brand": "NOKIA",
+        "image": "images/1.jpg"
+    }, {
+        "name": "Techno Spark S4",
+        "detail": "Feature-packed flagship from NOKIA",
+        "price": "23000",
+        "description": "Snapdragon 888",
+        "brand": "NOKIA",
+        "image": "images/2.jpg"
+    }
+]
---
+++ max.js
@@ -0,0 +1,17 @@
+var pro=[
+
+    {
+        "name":"ihopne",
+        "detail":"jkkhdhjkh",
+        "price":"12900"
+    },
+    {
+        "name":"nokia",
+        "detail":"jkkhdhjkh",
+        "price":"12900"
+    },
+    {},
+    {}
+
+
+]
